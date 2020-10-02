e40c5876ea9fc965398a5f0fe2c9da8319d9d11c
Mark Birchall <mark.birchall@gmail.com>
2020-10-02 12:17:07 +0100

    neatening

--- morse.js
+++ morse.js
@@ -19,9 +19,6 @@ let morseCodeNumbersArr = ["-----", ".----", "..---", "...--", "....-", ".....",
 englishArr = englishArr.concat(englishNumbersArr);
 morseCodeArr = morseCodeArr.concat(morseCodeNumbersArr);
 
-
-
-
 // add onclick to translate button
 let translateButton = document.getElementById("translate");
 translateButton.addEventListener("click", checkLanguage);
@@ -60,37 +57,44 @@ function checkLanguage() {
 
 }
 
+
+// translate english input to morse code output
 function translateEnglishToMorse(stringToConvert) {
 
-    // create an empty string to store the translation
     let outputString = "";
 
-    // loop through the input string
-    for (let i = 0; i < stringToConvert.length; i++) {
+    // make array from input
+    let arrayToConvert = stringToConvert.split("");
 
-        // loop through the english letters array
-        englishArr.forEach((letter, index) => {
+    // loop through input array 
+    arrayToConvert.forEach((sequence,position) => {
 
-            // if the input string letter matches an entry in the english array
-            if (letter == stringToConvert[i]) {
-                // add the corresponding morse code to the output string
+        // compare entries against morse code array
+        englishArr.forEach((char, index) => {
+
+            if (char == sequence) {
+                // build output string
                 outputString += morseCodeArr[index];
 
                 // check if we need to add a "/" to separate letters
                 // ie. input was longer than 1 letter and we are not at the end
-                if (stringToConvert.length > 1 && i < stringToConvert.length - 1) {
+                if (arrayToConvert.length > 1 && position < arrayToConvert.length - 1) {
                     // add a "/" to the end of the string
                     outputString += " / ";
                 }
             }
+
         });
-    }
+
+    });
 
     // write the translated string to the output area
     writeToOutput(outputString);
 
 }
 
+
+// translate morse code input to english output
 function translateMorseToEnglish(stringToConvert) {
 
     let outputString = "";
@@ -107,6 +111,7 @@ function translateMorseToEnglish(stringToConvert) {
             if (char == sequence) {
                 // build output string
                 outputString += englishArr[index];
+
             }
         });
 
@@ -117,10 +122,12 @@ function translateMorseToEnglish(stringToConvert) {
 
 }
 
+
+// write output to output area
 function writeToOutput(stringToOutput) {
 
     if(stringToOutput==""){
-        stringToOutput = "Unable to convert"
+        stringToOutput = "Unable to convert. Please use English or International Morse Code"
     }
 
     document.getElementById("output").value = stringToOutput;
