45c403b8b2827d18e0b5bd992e0b4465c47338de
Mark Birchall <mark.birchall@gmail.com>
2020-10-02 11:01:00 +0100

    commenting code

--- morse.js
+++ morse.js
@@ -1,68 +1,75 @@
+///////////////////////////////////////////////////
+// english / morse code translator
+///////////////////////////////////////////////////
+//
+//
 
 
-let english = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V" ,"W", "X", "Y", "Z"];
+// array of english letters
+let englishLettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// array of morse code letters
+let morseCodeArr = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
 
 
-
-
-//let morse =".- / -... / -.-. / -.. / . / ..-. / --. / .... / .. / .--- / -.- / .-.. / -- / -. / --- / .--. / --.- / .-. / ... / - / ..- / ...- / .-- / -..- / -.-- / --..";
-//let morseFIX = morse.split(" / ").join(`", "`);
-//console.log(morseFIX)
-
-let morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
-
-
-let MorseExp = 0;
+//let MorseExp = 0;
 
 // add onclick to translate button
 let translateButton = document.getElementById("translate");
-
 translateButton.addEventListener("click", checkLanguage);
 
-//console.log(textInput);
 
-function checkLanguage(){
+// check if user has inputted english or morse code
+function checkLanguage() {
 
+    // reg exp to look for letters
     let lettersExp = /[a-zA-Z]/g;
+
+    // get contents of input textarea
     let textInput = document.getElementById("input").value;
+
+    // convert input to uppercase
     textInput = textInput.toUpperCase();
-            
-    if(lettersExp.test(textInput)){
-        
-        console.log(textInput);
+
+    // check if input contains letters
+    if (lettersExp.test(textInput)) {
+
+        // if it does then call the english to morse translation function
         translateEnglishToMorse(textInput);
-        console.log("english");
+
     } else {
-        translateMorseToEnglish();
+        // if it doesn't then call the morse to english translation
+        translateMorseToEnglish(textInput);
     }
 
 }
 
-function translateEnglishToMorse(stringToConvert){
+function translateEnglishToMorse(stringToConvert) {
 
-   // match each char against the english string to find index
+    // create an empty string to store the translation
+    let outputString = "";
 
-    let morseString = "";
+    // loop through the input string
+    for (let i = 0; i < stringToConvert.length; i++) {
 
-   for(let i=0;i<stringToConvert.length; i++){
+        // loop through the english letters array
+        englishLettersArr.forEach((letter, index) => {
 
-    english.forEach((letter,index)=>{
+            // if the input string letter matches an entry in the english array
+            if (letter == stringToConvert[i]) {
+                // add the corresponding morse code to the output string
+                outputString += morseCodeArr[index];
 
- 
-    if(letter==stringToConvert[i]){
-        morseString += morseCode[index];
-        if(stringToConvert.length>1 && i<stringToConvert.length-1){
-            morseString += " / ";
-        }
+                // check if we need to add a "/" to separate letters
+                // ie. input was longer than 1 letter and we are not at the end
+                if (stringToConvert.length > 1 && i < stringToConvert.length - 1) {
+                    // add a "/" to the end of the string
+                    outputString += " / ";
+                }
+            }
+        });
     }
 
-    });
-
-
-   }
-
-console.log(morseString);
-
- 
+    // write the translated string to the output area
+    document.getElementById("output").value = outputString;
 
 }
